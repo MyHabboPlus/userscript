335b578357e57c9203b87a09a6c0cf55c8af5004
iButtonH <ibuttonhabbo@gmail.com>
2013-12-11 22:03:32 +0100

    Release 1.1a7

--- MyHabboPlus.user.js
+++ MyHabboPlus.user.js
@@ -3,7 +3,7 @@
 // @name        MyHabboPlus
 // @namespace   http://myhabboplus.com
 // @description Userscript for Habbo Hotel
-// @version     1.1a6
+// @version     1.1a7
 // @author		iButton
 // @author		Tetule
 
