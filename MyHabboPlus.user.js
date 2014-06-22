a13fa67b8ddf3548fac22d480efaf6292fbf4a79
iButtonH <ibuttonhabbo@gmail.com>
2014-06-22 14:07:13 +0200

    Release 1.1.0b2
    [FIXED] Bug in Badges Widget

--- MyHabboPlus.user.js
+++ MyHabboPlus.user.js
@@ -3,7 +3,7 @@
 // @name        MyHabboPlus
 // @namespace   http://myhabboplus.com
 // @description Userscript for Habbo Hotel
-// @version     1.1.0b1
+// @version     1.1.0b2
 // @author		iButton
 // @author		Tetule
 
