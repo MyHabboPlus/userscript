f8447a4c553ddccf855e78b0846b42deb44daefd
iButtonH <ibuttonhabbo@gmail.com>
2015-01-01 14:07:59 +0100

    [CHANGED] MyHabboPlus new logo

--- MyHabboPlus.user.js
+++ MyHabboPlus.user.js
@@ -3,7 +3,7 @@
 // @name        MyHabboPlus
 // @namespace   http://myhabboplus.com
 // @description Userscript for Habbo Hotel
-// @version     1.2.2a1
+// @version     1.2.3a1
 // @author		iButton
 // @author		Tetule
 
